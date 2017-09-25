de1381f5c6322544f912fe8010efd394e1a86273
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-09-25 08:36:27 -0600

    Played around with variables

--- IntroToSwift.playground/Contents.swift
+++ IntroToSwift.playground/Contents.swift
@@ -2,4 +2,38 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//Explicit Type declaration with no value
+var ramen : String
+
+//Implicit Type declaration with a value to assign type.
+var soup = "Ramen"
+
+ramen = soup
+print(ramen)
+print(soup)
+
+//ramen = 7
+
+var myAge : Int
+myAge = -89
+print(myAge)
+myAge = myAge + 654987354
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+print(myAge)
+
+var isReal: Bool
+isReal = false
+isReal = true
+
+let myName : String
+myName = "Brandon"
+print(myName)
+
+var eggs : String
+eggs = "I reallly want to eat fried eggs right now."
+print(eggs)
+
+var numberOfEggsICouldEat : Int
+numberOfEggsICouldEat = 328
+print(numberOfEggsICouldEat)
