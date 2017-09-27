42147d1e68ed5ad5151b837dc71beca3c552f59f
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-09-27 08:53:37 -0600

    Adding more methods and some logic

--- WorkingWithMethods.playground/Contents.swift
+++ WorkingWithMethods.playground/Contents.swift
@@ -28,3 +28,39 @@ evenLessSimpleMethod(count : 56,
                bandName : "Forgien Figures",
                date : "July 29, 2017.")
 
+public func calculateDays(first firstDay : Int,
+                          secondDay : Int) -> Int
+{
+    let answer = secondDay - firstDay
+    return answer
+}
+
+calculateDays(first: 4,
+              secondDay : 5)
+
+public func name(friendName homeName : String) -> Void
+{
+    let info = "At home my name is " + homeName
+    print(info)
+}
+
+name(friendName : "Australia")
+
+var guess: Double = 1
+if(guess > 500)
+{
+    print("Wahoo")
+}
+else if (guess == 65)
+{
+    print("Hi")
+}
+else
+{
+    print("Not likely")
+}
+while(guess > 0)
+{
+    print("HAHAHAHAHAHAHAHAHAHAHAHAHA                                                                                   \(guess)")
+    guess += 1
+}
